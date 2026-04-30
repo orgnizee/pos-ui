9135d85408fef75734d563e2cf8d3669cb2ca2e1
Hugo Belém <belem.hugo@hotmail.com>
2026-04-30 16:15:57 -0300

    chore: adapt personal use

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,37 +1,72 @@
+import { isApiError } from "@/lib/api/types";
+import { getUser } from "@/lib/api/user";
 import Link from "next/link";
 
 export default async function Home() {
+  const user = await getUser();
+
+  if (isApiError(user)) {
+    return;
+  }
+
   return (
     <main className="flex flex-col mt-12 text-6xl sm:text-8xl font-light">
-      <Link href={"/pdv"}>
-        <span className="text-2xl">1</span>
-        pdv
-      </Link>
+      {user.username !== "hugo" && (
+        <>
+          <Link href={"/pdv"}>
+            <span className="text-2xl">1</span>
+            pdv
+          </Link>
 
-      <Link href={"/caixa"}>
-        <span className="text-2xl">2</span>
-        caixa
-      </Link>
+          <Link href={"/caixa"}>
+            <span className="text-2xl">2</span>
+            caixa
+          </Link>
 
-      <Link href={"/fiados"}>
-        <span className="text-2xl">3</span>
-        fiados
-      </Link>
+          <Link href={"/fiados"}>
+            <span className="text-2xl">3</span>
+            fiados
+          </Link>
 
-      <Link href={"/contatos"}>
-        <span className="text-2xl">4</span>
-        contatos
-      </Link>
+          <Link href={"/contatos"}>
+            <span className="text-2xl">4</span>
+            contatos
+          </Link>
 
-      <Link href={"/vendas"}>
-        <span className="text-2xl">5</span>
-        vendas
-      </Link>
+          <Link href={"/vendas"}>
+            <span className="text-2xl">5</span>
+            vendas
+          </Link>
 
-      <Link href={"/produtos"}>
-        <span className="text-2xl">6</span>
-        produtos
-      </Link>
+          <Link href={"/produtos"}>
+            <span className="text-2xl">6</span>
+            produtos
+          </Link>
+        </>
+      )}
+      {user.username === "hugo" && (
+        <>
+          <Link href={"/caixa"}>
+            <span className="text-2xl">2</span>
+            caixa
+          </Link>
+
+          <Link href={"/fiados"}>
+            <span className="text-2xl">3</span>
+            recer
+          </Link>
+
+          <Link href={"/pagamentos"}>
+            <span className="text-2xl">4</span>
+            pagar
+          </Link>
+
+          <Link href={"/contatos"}>
+            <span className="text-2xl">5</span>
+            contatos
+          </Link>
+        </>
+      )}
     </main>
   );
 }
