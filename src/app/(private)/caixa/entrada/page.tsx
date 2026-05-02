3f402f5421c0a0ef1adc57805eccaac3640ee552
Hugo Belém <belem.hugo@hotmail.com>
2026-05-02 10:13:26 -0300

    chore: resize caixa transactions header

--- src/app/(private)/caixa/entrada/page.tsx
+++ src/app/(private)/caixa/entrada/page.tsx
@@ -27,7 +27,7 @@ export async function AddCreditPayload() {
   return (
     <section className="mt-6">
       <BackButton />
-      <p className="mt-8 text-6xl text-start font-light">entrada</p>
+      <p className="mt-8 sm:text-6xl text-5xl text-start font-light">entrada</p>
 
       <div className="relative flex items-center justify-center mt-2 ml-auto mr-auto w-full">
         <TransactionForm
--- src/app/(private)/caixa/saida/page.tsx
+++ src/app/(private)/caixa/saida/page.tsx
@@ -27,7 +27,7 @@ export async function AddDebitPayload() {
   return (
     <section className="mt-6">
       <BackButton />
-      <p className="mt-8 text-6xl text-start font-light">saída</p>
+      <p className="mt-8 sm:text-6xl text-5xl text-start font-light">saída</p>
 
       <div className="relative flex items-center justify-center mt-2 ml-auto mr-auto w-full">
         <TransactionForm
